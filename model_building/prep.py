12f00d6d29eca433a118eda78a82b74ca931b3aa
Gitamoreno <albertomoreno33@gmail.com>
2026-09-05 23:20:38 -0600

    update model_building/prep.py

--- model_building/prep.py
+++ model_building/prep.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv("/content/drive/MyDrive/MLOPs/MLS2/data/machine-failure-prediction.csv")
+#df = pd.read_csv("/content/drive/MyDrive/MLOPs/MLS2/data/machine-failure-prediction.csv")
+df = pd.read_csv("data/machine-failure-prediction.csv")
 df.drop(columns=["UDI"], inplace=True)
 
 # NOTE: 'Type' is intentionally left as raw strings (H/L/M).
